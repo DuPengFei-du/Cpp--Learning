f5738e4471dfcd28731cd697022ac4241ad15356
DuPengFei-du <935820240@qq.com>
2020-05-11 07:40:55 +0800

    test

---
+++ Cpp(17)/Cpp(17)/main.cpp
@@ -0,0 +1,221 @@
+//#include<iostream>
+//using namespace std;
+//struct Student
+//{
+//	char name[20];
+//	int age;
+//	char gender;
+//};
+////再C语言中的结构体，是只能放数据，不能放函数的，如果再C语言的前提下
+////给结构体中放函数，那么编译的时候是会出错的，之能放数据
+////因为C语言是面向过程的，数据和方法是分离开的
+////但是在C++中，struct声明的结构体，不仅可以放数据还是可以放函数的
+////也就是说C++把这些数据和操作这些数据的方法绑在一起了
+//int main()
+//{
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//struct Student
+//{
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1, s2, s3;  //学生对象(学生实体)
+//	s1.InitStudent("wq", 35, "男");
+//	s1.PrintStudnet();
+//
+//	s2.InitStudent("ouhou", 5, "女");
+//	s2.PrintStudnet();
+//
+//	s3.InitStudent("aha", 15, "男");
+//	s3.PrintStudnet();
+//
+//	return 0;
+//}
+
+
+//class Student
+//{
+//	//成员函数(也成为行为)
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//	//数据(也成为属性)
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+
+
+////person.h
+//
+//#pragma once 
+//class Person
+//{
+//	void InitPerson(char* name, char* gender, int age);
+//	void PrintPerson();
+//
+//	char name[20];
+//	char gender[3];
+//	int age;
+//};
+
+
+//Person.cpp
+
+//#include"Person.h"
+//
+//#include<string.h>
+//#include<iostream>
+//using namespace std;
+//
+//void Person::InitPerson(char* name, char* gender, int age)
+//{
+//	strcpy_s(_name, name);
+//	_age = age;
+//	strcpy_s(_gender, gender);
+//}
+//void Person::PrintPerson()
+//{
+//	cout << _name << "-" << _age << "-" << _gender << endl;
+//}
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Student
+//{
+//	//成员函数(也成为行为)
+//public:
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//private:
+//	//数据(也成为属性)
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1, s2, s3;  //学生对象(学生实体)
+//	s1.InitStudent("wq", 35, "男");
+//	s1.PrintStudnet();
+//
+//	s2.InitStudent("nana", 5, "女");
+//	s2.PrintStudnet();
+//
+//	s3.InitStudent("aha", 15, "男");
+//	s3.PrintStudnet();
+//
+//	return 0;
+//}
+
+
+//class Person
+//{
+//public:
+//	void PrintPersonInfo();
+//private:
+//	char _name[20];
+//	char _gender[3];
+//	int _age;
+//};
+//// 这里需要指定PrintPersonInfo是属于Person这个类域
+//void Person::PrintPersonInfo()
+//{
+//	cout << _name << " "_gender << " " << _age << endl;
+//}
+
+
+//class A1
+//{
+//public:
+//	void f1()
+//	{
+//
+//	}
+//private:
+//	int _a;
+//};
+//// 类中仅有成员函数
+//class A2
+//{
+//public:
+//	void f2()
+//	{
+//
+//	}
+//};
+//// 类中什么都没有---空类
+//class A3
+//{
+//
+//};
+
+
+//用C语言的方式进行模拟
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//struct Student
+//{
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//
+//void StudentInit(Student* ps, char* name, int age, char* gender)
+//{
+//	strcpy(ps->_name, name);
+//	ps->_age = age;
+//	strcpy(ps->_gender, gender);
+//}
+//
+//void PrintStudent(Student* ps)
+//{
+//	printf("%s %d %s", ps->_name, ps->_age, ps->_gender);
+//}
+//
+//int main()
+//{
+//	return 0;
+//}
