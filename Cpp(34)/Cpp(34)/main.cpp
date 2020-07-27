77693c9a55c87b2335ab01076d210e22fe721838
DuPengFei-du <935820240@qq.com>
2020-07-27 08:41:52 +0800

    test

---
+++ Cpp(34)/Cpp(34)/main.cpp
@@ -0,0 +1,418 @@
+////缺省参数的分类
+////全缺省参数和半缺省参数
+//void TestFunc(int a = 1, int b = 2, int c = 3)
+//{
+//	//所有参数都有缺省值称为全缺省参数
+//	cout << a << ' ' << b << ' ' << c << ' ' << endl;
+//}
+//
+////半缺省参数
+////void TestFunc2(int a=1,int b=2,int c)   
+////无法通过编译，把b的值拿掉，也无法通过编译
+////只有把a的值拿掉，才能通过编译
+////a和b都拿掉，有c也是可以的
+////得出规律，注意：半缺省参数只能从右往左一次给出
+//void TestFunc2(int a, int b, int c = 3)  //可以通过编译
+//{
+//	//部分参数带有缺省值
+//	cout << a << ' ' << b << ' ' << c << ' ' << endl;
+//}
+//int main()
+//{
+//	//全缺省
+//	TestFunc();  //打印 1 2 3
+//	TestFunc(10);  //打印 10 2 3 
+//	TestFunc(10, 20);  //打印10 20 3
+//	TestFunc(10, 20, 30);  //打印10 20 30
+//
+//	//半缺省
+//	TestFunc2(10, 20);
+//	TestFunc2(10, 20, 30);
+//
+//	return 0;
+//}
+
+
+//void TestFunc()
+//{
+//
+//}
+//
+//void TestFunc(int a)
+//{
+//
+//}
+////上面两个函数形成了重载
+//int main()
+//{
+//	return 0;
+//}
+//
+//void TestFunc(double a)
+//{
+//
+//}
+//
+//void TestFunc(int a)
+//{
+//
+//}
+////上面两个函数形成了重载
+//int main()
+//{
+//	return 0;
+//}
+//
+//void TestFunc(double a, int b)
+//{
+//
+//}
+//
+//void TestFunc(int a, double b)
+//{
+//
+//}
+////上面两个函数形成了重载
+//int main()
+//{
+//	return 0;
+//}
+
+
+//#include<iostream >
+//using namespace std;
+//
+//void Swap(int a, int b)
+//{
+//	cout << &a << " " << &b << " " << endl;
+//	int temp = a;
+//	a = b;
+//	b = temp;
+//	//交换的是形参的内容，不是实参的内容
+//}
+//
+//void Swap(int* pa, int* pb)
+//{
+//	cout << &pa << " " << &pb << " " << endl;
+//	int temp = *pa;
+//	*pa = *pb;
+//	*pb = temp;
+//}
+//
+////两个swap形成了重载
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	cout << &a << " " << &b << " " << endl;
+//	//Swap(a, b);  //如果只是值传递的话，是不行的
+//	Swap(&a, &b);
+//	cout << a << " " << b << " " << endl;
+//}
+
+
+//#include<iostream>
+//using namespace std;
+//inline int Add(int left, int right)
+//{
+//	return left + right;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	cout << Add(a, b) << endl;    //替换发生在编译阶段
+//	cout << Add(a, ++b) << endl;    //替换发生在编译阶段
+//	//因为替换发生在编译阶段，所以他会参与到函数编译里面去，代码运行效率会比较高
+//	//看有没有展开就看反汇编代码有没有直接调用这个函数就可以了
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//struct Student
+//{
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1, s2, s3;  //学生对象(学生实体)
+//	s1.InitStudent("wq", 35, "男");
+//	s1.PrintStudnet();
+//
+//	s2.InitStudent("ouhou", 5, "女");
+//	s2.PrintStudnet();
+//
+//	s3.InitStudent("aha", 15, "男");
+//	s3.PrintStudnet();
+//
+//	return 0;
+//}
+
+
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//class Student
+//{
+//	//成员函数(也成为行为)
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//	//数据(也成为属性)
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+
+
+//#include<iostream>
+//#include<string>
+//using namespace std;
+//class Student
+//{
+//	//成员函数(也成为行为)
+//public:
+//	void InitStudent(const char* name, int age, const char* gender)
+//	{
+//		strcpy(_name, name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//private:
+//	//数据(也成为属性)
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1, s2, s3;  //学生对象(学生实体)
+//	s1.InitStudent("wq", 35, "男");
+//	s1.PrintStudnet();
+//
+//	s2.InitStudent("nana", 5, "女");
+//	s2.PrintStudnet();
+//
+//	s3.InitStudent("aha", 15, "男");
+//	s3.PrintStudnet();
+//
+//	return 0;
+//}
+
+
+//class A1
+//{
+//public:
+//	void f1()
+//	{
+//
+//	}
+//private:
+//	int _a;
+//};
+//// 类中仅有成员函数
+//class A2
+//{
+//public:
+//	void f2()
+//	{
+//
+//	}
+//};
+//// 类中什么都没有---空类
+//class A3
+//{
+//
+//};
+
+
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//class Date
+//{
+//public:
+//	void Display()
+//	{
+//		cout << _year << "-" << _month << "-" << _day << endl;
+//	}
+//	void SetDate(int year, int month, int day)
+//	{
+//		_year = year;
+//		_month = month;
+//		_day = day;
+//	}
+//private:
+//	int _year; // 年
+//	int _month; // 月
+//	int _day; // 日
+//};
+//int main()
+//{
+//	Date d1, d2;
+//	d1.SetDate(2018, 5, 1);
+//	d2.SetDate(2018, 7, 1);
+//	d1.Display();
+//	d2.Display();
+//	return 0;
+//}
+
+
+//class Student
+//{
+//public:
+//	//this:  Student* const   这是this指针的类型，虽然this指向的内容是可以修改的，但是this本身是不可以被修改的
+//	void InitStudent(char* _name, int age, char* gender)
+//	{
+//		//this = nullptr;
+//		strcpy(this->_name, _name);
+//		_age = age;
+//		strcpy(_gender, gender);
+//	}
+//
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//private:
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1;
+//	s1.InitStudent("Peter", 35, "男");
+//	s1.PrintStudnet();
+//
+//	Student s2;
+//	s2.InitStudent("jingjing", 36, "女");
+//	s2.PrintStudnet();
+//	return 0;
+//}
+
+
+////用C语言的方式进行模拟
+//#include<iostream>
+//#include<string.h>
+//using namespace std;
+//struct Student
+//{
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//
+//void StudentInit(Student* ps, char* name, int age, char* gender)
+//{
+//	strcpy(ps->_name, name);
+//	ps->_age = age;
+//	strcpy(ps->_gender, gender);
+//}
+//
+//void PrintStudent(Student* ps)
+//{
+//	printf("%s %d %s", ps->_name, ps->_age, ps->_gender);
+//}
+//
+//int main()
+//{
+//	return 0;
+//}
+
+
+///*
+//大致过程：
+//1. 识别类名
+//2. 识别类中成员变量
+//3. 识别类中的成员函数&改写
+//*/
+//
+//#if 0
+//class Student
+//{
+//public:
+//	/*
+//	void InitStudent(Student* const this, char* name, int age, char* gender)
+//	{    看起来成员函数有三个参数，实际上是有四个参数的，因为还有一个this指针
+//		strcpy(this->_name, name);
+//		this->_age = age;
+//		strcpy(this->_gender, gender);
+//	}
+//	*/
+//	void InitStudent(char* _name, int age, char* gender);
+//	// 	{
+//	// 		strcpy(this->_name, _name);    在编译器中实际的样子
+//	// 		_age = age;  
+//	// 		strcpy(_gender, gender);
+//	// 	}
+//
+//	void TestFunc(...);
+//	// 	{
+//	// 	}
+//
+//		/*
+//		// this是编译器自己维护(不是由我们来维护的)
+//		void PrintStudnet(Student* const this)
+//		{
+//			cout << this->_name << "-" << this->_age << "-" << this->_gender << endl;
+//		}
+//		*/
+//	void PrintStudnet()
+//	{
+//		cout << _name << "-" << _age << "-" << _gender << endl;
+//	}
+//
+//private:
+//	char _name[20];
+//	int _age;
+//	char _gender[3];
+//};
+//int main()
+//{
+//	Student s1;
+//	s1.TestFunc();
+//	s1.TestFunc(10);
+//	s1.TestFunc(10, 20);
+//	// Student::InitStudent(&s1, "Peter", 35, "男")
+//	s1.InitStudent("Peter", 35, "男");
+//	// Student::PrintStudnet(&s1);
+//	s1.PrintStudnet();
+//
+//	Student s2;
+//	s2.InitStudent("jingjing", 36, "女");
+//	s2.PrintStudnet();
+//	return 0;
+//}
