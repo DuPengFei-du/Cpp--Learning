08311d21df48b7288f0800a92604c88dad761270
DuPengFei-du <935820240@qq.com>
2020-10-31 14:02:22 +0800

    test

---
+++ 类和对象(16)/类和对象(16)/main.cpp
@@ -0,0 +1,803 @@
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//无参的构造函数
+	Date()   //构造函数  ，没有返回值
+	{
+		cout << "Date()" << this << endl;   //打印this是为了看当前构造的是哪一个对象
+		//把对象的地址打印出来
+	}
+	Date(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	//两个构造函数形成了重载
+
+	//创建几个对象，编译器就会调用几次构造韩素
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1, d2, d3;
+	Date d4(2020, 4, 27);
+	Date d5();
+	//并不是创建了一个对象
+	//这是一个函数声明，相当于我是有一个函数名称
+	//为d5的函数，这个函数的返回值类型是Date类型，没有参数
+
+
+	d1.InitDate(2020, 4, 30);
+	d1.PrintDate();
+
+	d2.InitDate(2020, 4, 29);
+	d2.PrintDate();
+
+	d3.InitDate(2020, 4, 28);
+	d3.PrintDate();
+
+	d4.PrintDate();
+
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//无参的构造函数
+	Date()   //构造函数  ，没有返回值
+	{
+		cout << "Date()" << this << endl;   //打印this是为了看当前构造的是哪一个对象
+		//把对象的地址打印出来
+	}
+
+	//全缺省的构造函数
+	Date(int year = 2000, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	//两个构造函数形成了重载
+
+	//创建几个对象，编译器就会调用几次构造韩素
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1;
+}
+
+
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+class SeqList
+{
+public:
+	SeqList(int capacity = 10)
+	{
+		cout << "SeqList(int):" << this << endl;
+		array = (int*)malloc(sizeof(int) * capacity);
+		assert(array);
+		_capacity = capacity;
+		_size = 0;
+	}
+
+	//析构函数
+	~SeqList()
+	{
+		if (array)
+		{
+			free(array);
+			_capacity = 0;
+			_size = 0;
+		}
+		cout << "~SeqList():" << this << endl;
+	}
+private:
+	int* array;
+	int _size;
+	int _capacity;
+};
+void TestSeqList()
+{
+	SeqList s;
+}
+int main()
+{
+	SeqList s(100);
+
+	return 0;
+}
+
+
+int main()
+{
+	int a;
+	int b = 10;
+	int c(b);
+
+	string s1("hello");
+	string s2(s1);     //用s1去构造s2，然后s1和s2的内容就一模一样了
+	return 0
+}
+//但是通过监视可以看出，s1和s2的地址是一样的，也就是说s1和s2是同一块空间，那么在进行析构的时候就会出现问题，也就是说，同一块空间析构了两次
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//构造函数
+	Date(int year = 2020, int month = 5, int day = 5)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int ,int):" << this << endl;
+		//看构造的是哪一个对象
+	}
+
+	//拷贝构造函数
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		cout << "Date(const Date&d):" << this << endl;
+	}
+
+	//析构函数
+	~Date()
+	{
+		//对于日期类来说，这里面没有什么资源是需要去释放的
+		//所以对于日期类来说，给不给析构函数其实都没有什么影响
+		cout << "~Date():" << this << endl;
+		//看析构的是哪一个对象
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+void TestDate()
+{
+	Date d1(2020, 5, 5);
+	Date d2(d1);
+}
+int main()
+{
+	TestDate();
+	return 0;
+}
+
+
+#include<iostream>
+#include<stdlib.h>
+#include<string.h>
+using namespace std;
+class String
+{
+public:
+	String(const char* str = "")
+	{
+		cout << "String(const char* ):" << this << endl;
+		if (nullptr == str)
+			str = "";
+
+		_str = (char*)malloc(strlen(str) + 1);
+		strcpy(_str, str);
+	}
+
+	// 拷贝构造函数必须显式提供
+
+	~String()
+	{
+		cout << "~String():" << this << endl;
+		free(_str);
+		_str = nullptr;
+	}
+
+private:
+	char* _str;
+};
+
+void TestString()
+{
+	String s1("hello");
+	String s2(s1);
+}
+
+int main()
+{
+	TestString();
+	return 0;
+}
+
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		cout << "Date(Date&):" << this << endl;
+	}
+
+	// d1 = d2 = d3;
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)    //意思就是如果是自己给自己赋值的话，就不需要来做了
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
+
+		return *this;
+	}
+
+	// *this + day
+	// 问题：加完之后的结果可能是非法日期？？？
+	Date operator+(int day)
+	{
+		Date temp(*this);
+		temp._day += day;
+		return temp;
+	}
+	//先给一个临时变量temp，然后再去修改临时变量
+	//temp中内容，然后返回temp，要知道，现在不是以引用的方式返回的
+	//是以值得方式返回的
+
+
+	// 首先返回引用
+	// 返回引用是否可行---->引用作为函数的返回值类型：不能返回函数栈上的空间
+	Date& DateAdd(int day)
+	{
+		_day += day;
+		return *this;
+	}
+
+	bool operator==(const Date& d)
+	{
+		return _year == d._year &&
+			_month == d._month &&
+			_day == d._day;
+	}
+
+	bool operator!=(const Date& d)
+	{
+		return !(*this == d);
+	}
+
+	// 前置++
+	// d2 = ++d1
+	Date& operator++()
+	{
+		_day += 1;
+		return *this;
+	}
+
+	// 后置++
+	// d2 = d1++
+	Date operator++(int)
+	{
+		Date temp(*this);
+		_day += 1;
+		return temp;
+	}
+
+	Date& operator--()
+	{
+		_day -= 1;
+		return *this;
+	}
+
+	Date operator--(int)
+	{
+		Date temp(*this);
+		_day -= 1;
+		return temp;
+	}
+
+	~Date()
+	{
+		cout << "~Date():" << this << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+	int* _p;
+};
+
+// 不能重载非法运算符----
+// Date operator@(int a)
+// {
+// 	Date d;
+// 	return d;
+// }
+
+// int operator+(int left, int right)
+// {
+// 	return left + right;
+// }
+
+void TestDate()
+{
+	int a = 10;
+	int b = 20;
+	int c;
+	c = a + b;
+	a = b = c;
+
+	Date d1(2019, 3, 22);
+	d1.DateAdd(3);
+	d1 = d1 + 3;
+
+
+	Date d2(d1);
+	d2 = d1++;
+	d2 = ++d1;
+
+	Date d3(2018, 3, 22);
+	d3 = d3;
+	d3 = d1;
+	d3.operator=(d1);
+	d1 = d2 = d3;
+	d1.operator=(d2.operator=(d3));
+	if (d3 == d1)
+		;
+
+	Date& d4 = d3;
+
+	d4 = d3;
+}
+
+int main()
+{
+	TestDate();
+	return 0;
+}
+
+
+class Date
+{
+public:
+	Date(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year, int month, int day)
+		:_year(year),
+		_month(month),
+		_day(day)
+	{
+		cout << "Date(int year, int month, int day)" << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year, int month, int day)
+		:_year(year),
+		_month(month),
+		_day(day),
+		_day(day)
+	{
+		cout << "Date(int year, int month, int day)" << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(const Date& d)
+		: _year(d._year)
+		, _month(d._month)
+		, _day(d._day)
+	{
+		// 		_year = d._year;
+		// 		_month = d._month;
+		// 		_day = d._day;
+		cout << "Date(Date&):" << this << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+#if 0
+	// 初始化列表
+	Date(int year = 1900, int month = 1, int day = 1)
+		: _year(year)
+		, _month(month)
+		, _day(day)
+	{
+		// 构造函数体中是赋初值不是初始化
+// 		_year = year;
+// 		_month = month;
+// 		_day = day;
+		//r = _day;
+		cout << "Date(int,int,int):" << this << endl;
+	}
+#endif
+
+	// 初始化列表
+	// 1. 初始化列表中成员的出现次序，不代表其真正的初始化次序
+	// 2. 成员变量在初始化列表中的初始化次序为其在类中的声明次序
+	// 建议：最好不要使用成员初始化成员
+	Date(int year = 1900, int month = 1, int day = 1)
+		: _year(year)
+		, _month(month)
+		, _day(day)
+		, _r(_day)
+		, _a(_day)
+	{
+		// 构造函数体中是赋初值不是初始化
+		// 		_year = year;
+		// 		_month = month;
+		// 		_day = day;
+		//r = _day;
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+	Date(const Date& d)
+		: _year(d._year)
+		, _month(d._month)
+		, _day(d._day)
+		, _r(d._r)
+		, _a(d._a)
+	{
+		// 		_year = d._year;
+		// 		_month = d._month;
+		// 		_day = d._day;
+		cout << "Date(Date&):" << this << endl;
+	}
+
+	// d1 = d2 = d3;
+	Date& operator=(const Date& d)
+	{
+		cout << this << "=" << &d << endl;
+		if (this != &d)
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
+
+		return *this;
+	}
+
+	~Date()
+	{
+		cout << "~Date():" << this << endl;
+	}
+
+
+	int _year;
+	int _month;
+	int _day;
+	int& _r;
+	const int _a;
+};
+
+
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	Time(int hour, int minute, int second)
+		: _hour(hour)
+		, _minute(minute)
+		, _second(second)
+	{}
+
+
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+		: _year(year)
+		, _month(month)
+		, _day(day)
+		, _t(0, 0, 0)
+		//, _t()   //调用无参的默认的构造函数
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+	Time _t;
+	//如果没有上面的_t(0, 0, 0)，直接给出Time_t是会报错的，原因在于
+	//声明了一个time类的对象t，那么这个对象t一定是需要进行初始化的
+	//那么编译器会默认去Time类中寻找没有参数的构造函数，但是Time类此时是没有
+	//显示给出无参的构造函数的，所以就会出错
+	//那么我们为了可以正确的创建出来这个Time类类型的对象的话，那么我们就需要给出来一个有参数的
+	//就好比说_t(0, 0, 0)，就可通过编译了
+};
+
+int main()
+{
+	//Date d(2019, 3, 24);
+	Date d;
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	Time(int hour = 0, int minute = 0, int second = 0)
+		: _hour(hour)
+		, _minute(minute)
+		, _second(second)
+	{
+		cout << this->_hour << endl;
+	}
+
+	void TestFunc()
+	{}
+
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+		// 		: _year(year)
+		// 		, _month(month)
+		// 		, _day(day)
+				//, _t()   //调用无参的默认的构造函数
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+	Time _t;
+};
+
+
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	Time(int hour = 0, int minute = 0, int second = 0)
+		: _hour(hour)
+		, _minute(minute)
+		, _second(second)
+	{
+		cout << this << endl;    //我的意图是要去打印this，然后发现代码可以正常通过编译
+		//那么，也就是说，此时，我的对象已经构造好了
+	}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+
+#include<iostream>
+using namespace std;
+class Time
+{
+public:
+	Time(int hour = 0, int minute = 0, int second = 0)
+		: _hour(hour)
+		, _minute(minute)
+		, _second(second)
+	{
+		cout << this->_hour << endl;
+	}
+
+	void TestFunc()
+	{}
+
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+		// 		: _year(year)
+		// 		, _month(month)
+		// 		, _day(day)
+				//, _t()   //调用无参的默认的构造函数
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+	Time _t;
+};
+
+int main()
+{
+	// 在编译器编译期间，已经为main分配好了栈空间
+	// 该空间中已经包含了函数体中的局部对象
+	Date d;    // 在构造函数调用之前，对象是不存在的
+	Time t;
+	t.TestFunc();
+
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year)
+		: _year(year)
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+	Date& operator=(const Date& d)
+	{
+		return *this;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+int main()
+{
+	Date d(2019);
+	d = 2020;
+	//一个是日期类型的变量，一个是整形的变量，在我们之前想的是，这时不能通过编译的
+	//但是没想到，这样的代码是可以通过编译的，原因在于
+	// 2020---> 通过单参构造函数--->临时对象
+	//也就是说构造函数具有类型转换的功能，本来是一个int类型，然后被转换了
+	// 用一个整形变量给日期类型对象赋值
+	// 实际编译器背后会用2020构造一个无名对象，最后用无名对象给d1对象进行赋值
+	//但是一般情况下，会把这种类型转换禁止掉，那么如何来禁止呢？
+	//禁止的方法就是在构造函数前面加上一个explicit关键字
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	explicit Date(int year)
+		: _year(year)
+	{
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+	Date& operator=(const Date& d)
+	{
+		return *this;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+
+int main()
+{
+	Date d(2019);
+	d = 2020;   // 2020---> 通过单参构造函数--->临时对象
+	return 0;
+}
