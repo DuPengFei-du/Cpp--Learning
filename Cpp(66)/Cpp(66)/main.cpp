21cfd7e787a6cdb8650db73c3015aa20f1ab2c5c
DuPengFei-du <935820240@qq.com>
2021-01-31 23:14:15 +0800

    test

---
+++ Cpp(66)/Cpp(66)/main.cpp
@@ -0,0 +1,987 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+
+#include<iostream>
+using namespace std;
+struct Student
+{
+	char name[20];
+	int age;
+	char gender;
+};
+//再C语言中的结构体，是只能放数据，不能放函数的，如果再C语言的前提下
+//给结构体中放函数，那么编译的时候是会出错的，之能放数据
+//因为C语言是面向过程的，数据和方法是分离开的
+//但是在C++中，struct声明的结构体，不仅可以放数据还是可以放函数的
+//也就是说C++把这些数据和操作这些数据的方法绑在一起了
+int main()
+{
+	return 0;
+}
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+struct Student
+{
+	void InitStudent(const char* name, int age, const char* gender)
+	{
+		strcpy(_name, name);
+		_age = age;
+		strcpy(_gender, gender);
+	}
+
+	void PrintStudnet()
+	{
+		cout << _name << "-" << _age << "-" << _gender << endl;
+	}
+
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+int main()
+{
+	Student s1, s2, s3;  //学生对象(学生实体)
+	s1.InitStudent("wq", 35, "男");
+	s1.PrintStudnet();
+
+	s2.InitStudent("ouhou", 5, "女");
+	s2.PrintStudnet();
+
+	s3.InitStudent("aha", 15, "男");
+	s3.PrintStudnet();
+
+	return 0;
+}
+
+
+class Student
+{
+	//成员函数(也成为行为)
+	void InitStudent(const char* name, int age, const char* gender)
+	{
+		strcpy(_name, name);
+		_age = age;
+		strcpy(_gender, gender);
+	}
+
+	void PrintStudnet()
+	{
+		cout << _name << "-" << _age << "-" << _gender << endl;
+	}
+
+	//数据(也成为属性)
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+
+
+//person.h
+
+#pragma once 
+class Person
+{
+	void InitPerson(char* name, char* gender, int age);
+	void PrintPerson();
+
+	char name[20];
+	char gender[3];
+	int age;
+};
+
+
+//Person.cpp
+
+#include"Person.h"
+
+#include<string.h>
+#include<iostream>
+using namespace std;
+
+void Person::InitPerson(char* name, char* gender, int age)
+{
+	strcpy_s(_name, name);
+	_age = age;
+	strcpy_s(_gender, gender);
+}
+void Person::PrintPerson()
+{
+	cout << _name << "-" << _age << "-" << _gender << endl;
+}
+
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Student
+{
+	//成员函数(也成为行为)
+public:
+	void InitStudent(const char* name, int age, const char* gender)
+	{
+		strcpy(_name, name);
+		_age = age;
+		strcpy(_gender, gender);
+	}
+
+	void PrintStudnet()
+	{
+		cout << _name << "-" << _age << "-" << _gender << endl;
+	}
+private:
+	//数据(也成为属性)
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+int main()
+{
+	Student s1, s2, s3;  //学生对象(学生实体)
+	s1.InitStudent("wq", 35, "男");
+	s1.PrintStudnet();
+
+	s2.InitStudent("nana", 5, "女");
+	s2.PrintStudnet();
+
+	s3.InitStudent("aha", 15, "男");
+	s3.PrintStudnet();
+
+	return 0;
+}
+
+
+class Person
+{
+public:
+	void PrintPersonInfo();
+private:
+	char _name[20];
+	char _gender[3];
+	int _age;
+};
+// 这里需要指定PrintPersonInfo是属于Person这个类域
+void Person::PrintPersonInfo()
+{
+	cout << _name << " "_gender << " " << _age << endl;
+}
+
+
+class A1
+{
+public:
+	void f1()
+	{
+
+	}
+private:
+	int _a;
+};
+// 类中仅有成员函数
+class A2
+{
+public:
+	void f2()
+	{
+
+	}
+};
+// 类中什么都没有---空类
+class A3
+{
+
+};
+
+
+#include<iostream>
+#include<string.h>
+using namespace std;
+class Date
+{
+public:
+	void Display()
+	{
+		cout << _year << "-" << _month << "-" << _day << endl;
+	}
+	void SetDate(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+private:
+	int _year; // 年
+	int _month; // 月
+	int _day; // 日
+};
+int main()
+{
+	Date d1, d2;
+	d1.SetDate(2018, 5, 1);
+	d2.SetDate(2018, 7, 1);
+	d1.Display();
+	d2.Display();
+	return 0;
+}
+
+
+class Student
+{
+public:
+	//this:  Student* const   这是this指针的类型，虽然this指向的内容是可以修改的，但是this本身是不可以被修改的
+	void InitStudent(char* _name, int age, char* gender)
+	{
+		//this = nullptr;
+		strcpy(this->_name, _name);
+		_age = age;
+		strcpy(_gender, gender);
+	}
+
+	void PrintStudnet()
+	{
+		cout << _name << "-" << _age << "-" << _gender << endl;
+	}
+
+private:
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+int main()
+{
+	Student s1;
+	s1.InitStudent("Peter", 35, "男");
+	s1.PrintStudnet();
+
+	Student s2;
+	s2.InitStudent("jingjing", 36, "女");
+	s2.PrintStudnet();
+	return 0;
+}
+
+
+//用C语言的方式进行模拟
+#include<iostream>
+#include<string.h>
+using namespace std;
+struct Student
+{
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+
+void StudentInit(Student* ps, char* name, int age, char* gender)
+{
+	strcpy(ps->_name, name);
+	ps->_age = age;
+	strcpy(ps->_gender, gender);
+}
+
+void PrintStudent(Student* ps)
+{
+	printf("%s %d %s", ps->_name, ps->_age, ps->_gender);
+}
+
+int main()
+{
+	return 0;
+}
+
+
+/*
+大致过程：
+1. 识别类名
+2. 识别类中成员变量
+3. 识别类中的成员函数&改写
+*/
+
+#if 0
+class Student
+{
+public:
+	/*
+	void InitStudent(Student* const this, char* name, int age, char* gender)
+	{    看起来成员函数有三个参数，实际上是有四个参数的，因为还有一个this指针
+		strcpy(this->_name, name);
+		this->_age = age;
+		strcpy(this->_gender, gender);
+	}
+	*/
+	void InitStudent(char* _name, int age, char* gender);
+	// 	{
+	// 		strcpy(this->_name, _name);    在编译器中实际的样子
+	// 		_age = age;  
+	// 		strcpy(_gender, gender);
+	// 	}
+
+	void TestFunc(...);
+	// 	{
+	// 	}
+
+		/*
+		// this是编译器自己维护(不是由我们来维护的)
+		void PrintStudnet(Student* const this)
+		{
+			cout << this->_name << "-" << this->_age << "-" << this->_gender << endl;
+		}
+		*/
+	void PrintStudnet()
+	{
+		cout << _name << "-" << _age << "-" << _gender << endl;
+	}
+
+private:
+	char _name[20];
+	int _age;
+	char _gender[3];
+};
+int main()
+{
+	Student s1;
+	s1.TestFunc();
+	s1.TestFunc(10);
+	s1.TestFunc(10, 20);
+	// Student::InitStudent(&s1, "Peter", 35, "男")
+	s1.InitStudent("Peter", 35, "男");
+	// Student::PrintStudnet(&s1);
+	s1.PrintStudnet();
+
+	Student s2;
+	s2.InitStudent("jingjing", 36, "女");
+	s2.PrintStudnet();
+	return 0;
+}
+
+
+class A
+{
+public:
+	void TestFunc1()
+	{
+		cout << this << endl;
+		this->_a = 20;   //如果没有这句话的话，下面的代码是可以正常通过编译的
+		cout << "TestFunc()" << endl;
+	}
+
+	void TestFunc2()
+	{
+		this->_a = 10;
+	}
+
+protected:
+	int _a;
+};
+
+
+int g_a = 10;
+
+int main()
+{
+	cout << g_a << endl;
+	A* ps = nullptr;
+	ps->TestFunc1();// TestFunc1(ps)
+	return 0;
+}
+
+
+class Test
+{
+public:
+	void TestFunc()
+	{
+		//cout << &this << endl;
+		Test* const& p = this;   //这句话就相当于是在打印this指针的地址
+		//因为this指针不能直接打印地址，因为this指针是一个常量，所以打印
+		//this指针别名的地址
+		cout << &p << endl;
+	}
+
+public:
+	int _t;
+};
+
+int main()
+{
+	int* p = nullptr;   //给成空
+	int*& q = p;
+
+	cout << &q << endl;
+	cout << &p << endl;
+	//打印出来之后发现两个变量的地址是一样的，p和q的地址相同
+
+	Test t;
+	t.TestFunc();   //通过对象去调用函数
+
+	return 0;
+}
+//然后之后去监视ebp和esp两个寄存器的地址，发现this的地址刚好在ebp和esp
+//两个寄存器的地址之间
+
+
+class Date
+{
+	//看起来这个类的函数体里面好像什么都没有
+	//但是其实还有6个默认的成员函数
+};
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1, d2, d3;
+
+	d1.InitDate(2020, 4, 30);
+	d1.PrintDate();
+
+	d2.InitDate(2020, 4, 29);
+	d2.PrintDate();
+
+	d3.InitDate(2020, 4, 28);
+	d3.PrintDate();
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//无参的构造函数
+	Date()   //构造函数  ，没有返回值
+	{
+		cout << "Date()" << this << endl;   //打印this是为了看当前构造的是哪一个对象
+		//把对象的地址打印出来
+	}
+	Date(int year, int month, int day)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	//两个构造函数形成了重载
+
+	//创建几个对象，编译器就会调用几次构造韩素
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1, d2, d3;
+	Date d4(2020, 4, 27);
+	Date d5();
+	//并不是创建了一个对象
+	//这是一个函数声明，相当于我是有一个函数名称
+	//为d5的函数，这个函数的返回值类型是Date类型，没有参数
+
+
+	d1.InitDate(2020, 4, 30);
+	d1.PrintDate();
+
+	d2.InitDate(2020, 4, 29);
+	d2.PrintDate();
+
+	d3.InitDate(2020, 4, 28);
+	d3.PrintDate();
+
+	d4.PrintDate();
+
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//无参的构造函数
+	//Date()   //构造函数  ，没有返回值
+	//{
+	//	cout << "Date()" << this << endl;   //打印this是为了看当前构造的是哪一个对象
+	//	//把对象的地址打印出来
+	//}
+	//Date(int year, int month, int day)
+	//{
+	//	_year = year;
+	//	_month = month;
+	//	_day = day;
+	//}
+	//两个构造函数形成了重载
+
+	//创建几个对象，编译器就会调用几次构造韩素
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1;
+	//编译器生成的构造函数是默认的构造函数
+	//默认的构造函数一定是无惨的
+	//如果说，用户定义构造函数了，编译器就不会生成的
+	//如果这个时候再创建一个带有三个参数的对象的话
+	//就一定是不会创建成功的
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//无参的构造函数
+	Date()   //构造函数  ，没有返回值
+	{
+		cout << "Date()" << this << endl;   //打印this是为了看当前构造的是哪一个对象
+		//把对象的地址打印出来
+	}
+
+	//全缺省的构造函数
+	Date(int year = 2000, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	//两个构造函数形成了重载
+
+	//创建几个对象，编译器就会调用几次构造韩素
+	void InitDate(int year, int month, int day)   //进行初始化的操作
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+	}
+	void PrintDate()   //打印进行检测
+	{
+		cout << _year << "/" << _month << "/" << _day << endl;
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+
+int main()
+{
+	Date d1;
+}
+
+
+class Time
+{
+public:
+	Time()
+	{
+		cout << "Time()" << endl;
+		_hour = 0;
+		_minute = 0;
+		_second = 0;
+	}
+private:
+	int _hour;
+	int _minute;
+	int _second;
+};
+class Date
+{
+private:
+	// 基本类型(内置类型)
+	int _year;
+	int _month;
+	int _day;
+	// 自定义类型
+	Time _t;
+};
+int main()
+{
+	Date d;
+	return 0;
+}
+
+
+#include<iostream>
+#include<assert.h>
+using namespace std;
+
+class SeqList
+{
+public:
+	SeqList(int capacity = 10)
+	{
+		cout << "SeqList(int):" << this << endl;
+		array = (int*)malloc(sizeof(int) * capacity);
+		assert(array);
+		_capacity = capacity;
+		_size = 0;
+	}
+
+	//析构函数
+	~SeqList()
+	{
+		if (array)
+		{
+			free(array);
+			_capacity = 0;
+			_size = 0;
+		}
+		cout << "~SeqList():" << this << endl;
+	}
+private:
+	int* array;
+	int _size;
+	int _capacity;
+};
+void TestSeqList()
+{
+	SeqList s;
+}
+int main()
+{
+	SeqList s(100);
+
+	return 0;
+}
+
+
+int main()
+{
+	int a;
+	int b = 10;
+	int c(b);
+
+	string s1("hello");
+	string s2(s1);     //用s1去构造s2，然后s1和s2的内容就一模一样了
+	return 0
+}
+//但是通过监视可以看出，s1和s2的地址是一样的，也就是说s1和s2是同一块空间，那么在进行析构的时候就会出现问题，也就是说，同一块空间析构了两次
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	Date(int year = 2020, int month = 5, int day = 5)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int ,int):" << this << endl;
+		//看构造的是哪一个对象
+	}
+
+	~Date()
+	{
+		//对于日期类来说，这里面没有什么资源是需要去释放的
+		//所以对于日期类来说，给不给析构函数其实都没有什么影响
+		cout << "~Date():" << this << endl;
+		//看析构的是哪一个对象
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+int main()
+{
+	Date d1(2020, 5, 5);
+	Date d2(d1);
+
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//构造函数
+	Date(int year = 2020, int month = 5, int day = 5)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int ,int):" << this << endl;
+		//看构造的是哪一个对象
+	}
+
+	//拷贝构造函数
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		cout << "Date(const Date&d):" << this << endl;
+	}
+
+	//析构函数
+	~Date()
+	{
+		//对于日期类来说，这里面没有什么资源是需要去释放的
+		//所以对于日期类来说，给不给析构函数其实都没有什么影响
+		cout << "~Date():" << this << endl;
+		//看析构的是哪一个对象
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+void TestDate()
+{
+	Date d1(2020, 5, 5);
+	Date d2(d1);
+}
+int main()
+{
+	TestDate();
+	return 0;
+}
+
+
+#include<iostream>
+using namespace std;
+class Date
+{
+public:
+	//构造函数
+	Date(int year = 2020, int month = 5, int day = 5)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int ,int):" << this << endl;
+		//看构造的是哪一个对象
+	}
+
+	//析构函数
+	~Date()
+	{
+		//对于日期类来说，这里面没有什么资源是需要去释放的
+		//所以对于日期类来说，给不给析构函数其实都没有什么影响
+		cout << "~Date():" << this << endl;
+		//看析构的是哪一个对象
+	}
+private:
+	int _year;
+	int _month;
+	int _day;
+};
+void TestDate()
+{
+	Date d1(2020, 5, 5);
+	Date d2(d1);
+}
+int main()
+{
+	TestDate();
+	return 0;
+}
+
+
+class Date
+{
+public:
+	Date(int year = 1900, int month = 1, int day = 1)
+	{
+		_year = year;
+		_month = month;
+		_day = day;
+		cout << "Date(int,int,int):" << this << endl;
+	}
+
+	Date(const Date& d)
+	{
+		_year = d._year;
+		_month = d._month;
+		_day = d._day;
+		cout << "Date(Date&):" << this << endl;
+	}
+
+	// d1 = d2 = d3;
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)    //意思就是如果是自己给自己赋值的话，就不需要来做了
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
+
+		return *this;
+	}
+
+	// *this + day
+	// 问题：加完之后的结果可能是非法日期？？？
+	Date operator+(int day)
+	{
+		Date temp(*this);
+		temp._day += day;
+		return temp;
+	}
+	//先给一个临时变量temp，然后再去修改临时变量
+	//temp中内容，然后返回temp，要知道，现在不是以引用的方式返回的
+	//是以值得方式返回的
+
+
+	// 首先返回引用
+	// 返回引用是否可行---->引用作为函数的返回值类型：不能返回函数栈上的空间
+	Date& DateAdd(int day)
+	{
+		_day += day;
+		return *this;
+	}
+
+	bool operator==(const Date& d)
+	{
+		return _year == d._year &&
+			_month == d._month &&
+			_day == d._day;
+	}
+
+	bool operator!=(const Date& d)
+	{
+		return !(*this == d);
+	}
+
+	// 前置++
+	// d2 = ++d1
+	Date& operator++()
+	{
+		_day += 1;
+		return *this;
+	}
+
+	// 后置++
+	// d2 = d1++
+	Date operator++(int)
+	{
+		Date temp(*this);
+		_day += 1;
+		return temp;
+	}
+
+	Date& operator--()
+	{
+		_day -= 1;
+		return *this;
+	}
+
+	Date operator--(int)
+	{
+		Date temp(*this);
+		_day -= 1;
+		return temp;
+	}
+
+	~Date()
+	{
+		cout << "~Date():" << this << endl;
+	}
+
+private:
+	int _year;
+	int _month;
+	int _day;
+	int* _p;
+};
+
+// 不能重载非法运算符----
+// Date operator@(int a)
+// {
+// 	Date d;
+// 	return d;
+// }
+
+// int operator+(int left, int right)
+// {
+// 	return left + right;
+// }
+
+void TestDate()
+{
+	int a = 10;
+	int b = 20;
+	int c;
+	c = a + b;
+	a = b = c;
+
+	Date d1(2019, 3, 22);
+	d1.DateAdd(3);
+	d1 = d1 + 3;
+
+
+	Date d2(d1);
+	d2 = d1++;
+	d2 = ++d1;
+
+	Date d3(2018, 3, 22);
+	d3 = d3;
+	d3 = d1;
+	d3.operator=(d1);
+	d1 = d2 = d3;
+	d1.operator=(d2.operator=(d3));
+	if (d3 == d1)
+		;
+
+	Date& d4 = d3;
+
+	d4 = d3;
+}
+
+int main()
+{
+	TestDate();
+	return 0;
+}
